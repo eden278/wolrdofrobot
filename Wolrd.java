e075c114f8734d83fb73459510e5d08b2236e5e6
eden.duport <edenduport@gmail.com>
2024-11-20 16:52:03 +0100

    acajout

---
+++ Wolrd.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.awt.*;
+
+/**
+ * Décrivez votre classe Wolrd ici.
+ *
+ * @author (votre nom)
+ * @version (un numéro de version ou une date)
+ */
+public class Wolrd
+{
+    private String worldname;
+    private ArrayList<Robot> Listeofrobot; 
+    public int capacity;
+    private boolean IsRunning;
+    private int steps; 
+    private int size; 
+}
---
+++ robot.java
@@ -0,0 +1,19 @@
+
+/**
+ * Décrivez votre classe robot ici.
+ *
+ * @author (votre nom)
+ * @version (un numéro de version ou une date)
+ */
+public class robot
+{
+    // variables d'instance - remplacez l'exemple qui suit par le vôtre
+    private String name;
+    private int Xposition ; 
+    private int Yposition ; 
+    private String AttributeColor;
+    private boolean Visible;
+    private int MaxPosition ; 
+    private int MinPosition ;
+    
+}
